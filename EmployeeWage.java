17146716603f5592516c2d1d6d1ca32e5d0ca75e
DESKTOP-0KACA77\hp <priteshbhangale04@gmail.com>
2021-08-10 10:30:33 +0530

    Uc Pushed

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -23,6 +23,33 @@ public class EmployeeWage {
 			System.out.println("Employee Absent");
 		}
 		
+		if(flag==1)
+		{
+		int DailyWage= fullday*SalHour;
+		System.out.println("DailyWage is "+ DailyWage);
+		}
+			
+		int mn=0,mx=1;
+		int  choice = (int) (Math.random()*(mx-mn+1)+mn);
+		switch (choice) {
+		case 0 : int fullTime=8;
+		System.out.println("Fulltime wage is "+ fullTime*SalHour);
+		break;
+		case 1:int HalfTime=4;
+		System.out.println("Halftime wage is "+ HalfTime*SalHour);
+		break;
+
+		default : break;
+		}
+		//UC5
+		if(flag==1) {
+		int WageperMonth= fullday*20 * SalHour;
+		System.out.println("WageperMonth is " + WageperMonth);
+		}
+
+		
+		
+		
 		
 		
 	}
